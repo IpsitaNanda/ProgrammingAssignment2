25ee9a8d01da677e7c98a72e798915cf12aa4a6f
IpsitaNanda <ipsitananda08@gmail.com>
2015-03-22 11:20:29 +0530

    Update cachematrix.R
    Latest Code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## The functions below make use of the scoping rules of R to cache the long taking computations and store it in the parent environment of the function.
+## The value once computed for a matrix can be fetched multiple times from the cache
+
+## This function to give a list of the functions to be used to set and get the values and means of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                                minv <- NULL
+        set <- function(y) {
+                x <<- y
+                minv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) minv <<- inv
+        getinv <- function() minv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## A function to reuse the value in cache if found else compute the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+                                minv <- x$getinv()
+        if(!is.null(minv)) {
+                message("getting cached data")
+                return(minv)
+        }
+        data <- x$get()
+        minv <- solve(data, ...)
+        x$setinv(minv)
+        minv
 }
+
